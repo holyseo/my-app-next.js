e108b40ff995ff5694e0c4d023bb19952f2a67d5
holyseo <128103221+holyseo@users.noreply.github.com>
2024-02-08 20:18:36 +0000

    handle errors in progress

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,4 +1,10 @@
+"use client";
+
 import { notFound } from "next/navigation";
+
+function getRandomInt(count: number) {
+  return Math.floor(Math.random() * count);
+}
 
 export default function ReviewDetail({
   params,
@@ -8,6 +14,12 @@ export default function ReviewDetail({
     reviewId: string;
   };
 }) {
+  const random = getRandomInt(2);
+
+  if (random === 1) {
+    throw new Error("Error loading review");
+  }
+
   if (parseInt(params.reviewId) > 1000) {
     notFound();
   }
